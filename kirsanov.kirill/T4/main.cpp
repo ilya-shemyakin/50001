cec920a2d1c6f100889a766d9633226ed2d33d36
Kirsanov Kirill <kirill.kirsanov1995@gmail.com>
2026-03-25 21:14:00 +0300

    Fix formatting

--- kirsanov.kirill/T4/main.cpp
+++ kirsanov.kirill/T4/main.cpp
@@ -8,45 +8,45 @@
 
 int main()
 {
-std::vector<std::unique_ptr<Shape>> figures;
+	std::vector<std::unique_ptr<Shape>> figures;
 
-// Создание составной фигуры
-auto comp = std::make_unique<CompositeShape>();
+	// Создание составной фигуры
+	auto comp = std::make_unique<CompositeShape>();
 
-comp->addShape(
-std::make_unique<Rectangle>(Point(1, 2), Point(5, 7))
-);
+	comp->addShape(
+		std::make_unique<Rectangle>(Point(1, 2), Point(5, 7))
+	);
 
-comp->addShape(
-std::make_unique<Ring>(Point(2, 3), 5, 2)
-);
+	comp->addShape(
+		std::make_unique<Ring>(Point(2, 3), 5, 2)
+	);
 
-figures.push_back(std::move(comp));
+	figures.push_back(std::move(comp));
 
-// Добавляем отдельные фигуры
-figures.push_back(
-std::make_unique<Rectangle>(Point(0, 1), Point(4, 6))
-);
+	// Добавляем отдельные фигуры
+	figures.push_back(
+		std::make_unique<Rectangle>(Point(0, 1), Point(4, 6))
+	);
 
-figures.push_back(
-std::make_unique<Ring>(Point(3, 4), 6, 1)
-);
+	figures.push_back(
+		std::make_unique<Ring>(Point(3, 4), 6, 1)
+	);
 
-// Вывод до масштабирования
-std::cout << "Before scale:\n";
+	// Вывод до масштабирования
+	std::cout << "Before scale:\n";
 
-for (const auto& f : figures)
-f->printInfo();
+	for (const auto& f : figures)
+		f->printInfo();
 
-// Масштабирование в 2 раза
-for (auto& f : figures)
-f->scale(2);
+	// Масштабирование в 2 раза
+	for (auto& f : figures)
+		f->scale(2);
 
-// Вывод после масштабирования
-std::cout << "\nAfter scale:\n";
+	// Вывод после масштабирования
+	std::cout << "\nAfter scale:\n";
 
-for (const auto& f : figures)
-f->printInfo();
+	for (const auto& f : figures)
+		f->printInfo();
 
-return 0;
+	return 0;
 }
