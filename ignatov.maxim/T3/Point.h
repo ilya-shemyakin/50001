bfb7a65cb3e294696ae40b10b90ed2856740ce79
Ignatov Maxim <ignatov.mv@edu.spbstu.ru>
2026-05-21 02:06:29 +0300

    Fixed more trailing whitespace

--- ignatov.maxim/T3/Point.h
+++ ignatov.maxim/T3/Point.h
@@ -5,8 +5,8 @@ struct Point
     int x = 0;
     int y = 0;
     bool operator==(const Point& other) const { return x == other.x && y == other.y; }
-    bool operator<(const Point& other) const 
-    { 
+    bool operator<(const Point& other) const
+    {
         return x < other.x || (x == other.x && y < other.y
             );
     }
